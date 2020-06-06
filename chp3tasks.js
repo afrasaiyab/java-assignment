98ca8116d39fb389eed092ce903c5d9745983815
afrasaiyab <64470197+afrasaiyab@users.noreply.github.com>
2020-06-07 00:33:36 +0500

    Add files via upload

---
+++ chp3tasks.js
@@ -0,0 +1,21 @@
+// question # 1:
+var age=21;
+alert("I am"+" "+age+" "+"years old" )
+
+
+// question # 2:
+var vis=+prompt("enter a visited number:");
+alert("You have visited this site"+" "+vis+" "+"times")
+
+
+// question # 3:
+var birthyear=1999;
+document.write("My birth year is"+" "+birthyear +"<br>");
+document.write("Data type of my declared variable is number");
+
+
+// question # 4:
+var name="Raja";
+var product="T-shirt(s)";
+var quantity=5;
+document.write(name+" "+"ordered"+" "+quantity+" "+product+" "+"on XYZ Clothing store")
